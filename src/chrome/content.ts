ee100118afa4814f1fad1bc3866cfe48270ecec5
Romain THIVILLON <romain.thivillon@consulting-for.accor.com>
2024-02-29 23:15:41 +0100

    feat: update content script

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -1,4 +1,4 @@
-import { ChromeMessage, Sender } from "../type";
+import { ChromeMessage, Config, Sender } from "../type";
 
 type MessageResponse = (response?: any) => void
 
@@ -17,7 +17,7 @@ const messagesFromReactAppListener = (
 
     const isValidated = validateSender(message, sender);
 
-    if(message.type === 'SAVE_CONFIG') {
+    if (message.type === 'SAVE_CONFIG') {
         response('Config saved');
     }
 
@@ -29,6 +29,31 @@ const messagesFromReactAppListener = (
 
 const main = () => {
     console.log('[content.ts] Main')
+
+    const message: ChromeMessage = {
+        from: Sender.React,
+        type: "GET_CONFIG",
+    };
+    chrome.runtime.sendMessage(message).then((response: Config) => {
+        console.log('response', response)
+        window.addEventListener('resize', () => {
+            const globalHeader: HTMLElement | null = document.querySelector('[aria-label="Global"]');
+
+            if (globalHeader) {
+                const userElement = globalHeader.querySelector('[aria-controls="menu--account"]');
+                const user = userElement ? userElement.getAttribute('aria-label') : null;
+                console.log('user', user);
+                if (user) {
+                    const configKeys = Object.keys(response);
+                    const match = configKeys.filter(c => user.includes(c));
+
+                    if (match.length > 0) {
+                        globalHeader.style.backgroundColor = response[match[0]];
+                    }
+                }
+            }
+        })
+    });
     /**
      * Fired when a message is sent from either an extension process or a content script.
      */
